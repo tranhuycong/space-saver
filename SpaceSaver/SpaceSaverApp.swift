ba9f9a0630056854d5ceb53c0feebb340b100805
congtranit <congtranit@gmail.com>
2024-10-07 15:38:12 +0700

    Initial Commit

---
+++ SpaceSaver/SpaceSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceSaverApp.swift
+//  SpaceSaver
+//
+//  Created by Tran Cong on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SpaceSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpaceSaverTests/SpaceSaverTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceSaverTests.swift
+//  SpaceSaverTests
+//
+//  Created by Tran Cong on 7/10/24.
+//
+
+import Testing
+@testable import SpaceSaver
+
+struct SpaceSaverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
